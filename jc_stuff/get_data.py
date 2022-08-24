faa91962f16dee9aac61a98f448593380575c7b3
CrossOnline <stalwartgaming@gmail.com>
2022-08-24 07:04:26 -0500

    created a file to get data

---
+++ jc_stuff/get_data.py
@@ -0,0 +1,8 @@
+import requests
+from pprint import pprint
+result = requests.get("https://lichess.org/api")
+print(result.status_code)
+
+user = requests.get("https://lichess.org/api/user/cross_online")
+pprint(user.json())
+pprint(user['bio']['createdAt']["playTime": {"total"}])
